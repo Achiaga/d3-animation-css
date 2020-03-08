a20744afaa073573b9c612c85f462b0c5495e022
Achiaga <alfonso.achiaga@gmail.com>
2020-03-08 12:38:35 +0100

    add react-spring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import './App.css';
 import Navbar from './navBar';
+import Demo from './Demo';
+import { useSpring, animated } from 'react-spring';
+import './App.css';
 
 export default function App() {
+	const props = useSpring({
+		opacity: 1,
+		from: { opacity: 0 },
+	});
 	return (
 		<div className='App'>
 			<Navbar />
 			<h1 className='hello'>Welcome to Root</h1>
 			<div className='wrapper'>
-				Start editing to see something
+				Start editing to see some
 				<div className='slidingVertical'>
-					<span>magical</span>
-					<span>fantastical</span>
+					<span>fantastic</span>
+					<span>beautiful</span>
+					<span>magic</span>
 					<span>awesome</span>
-					<span>special</span>
 				</div>
 				<span className='last'>happen!</span>
 			</div>
 			<div id='type'>The brown fox jumped the lazy dog.</div>
+			<animated.h1 style={props}>hello</animated.h1>
+			<Demo />
 		</div>
 	);
 }
